5f63fb4dcb3ad9a2f8ac9a69e7fd57418ac3fd3a
Manuel Manatap <manuelp.sianturi@gmail.com>
2025-09-29 01:12:16 +0700

    Commit No. 1 - Retrofit Setup and Fetch API JSON Response

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,13 +36,18 @@ android {
 }
 
 dependencies {
-
+    // Default dependencies dari template
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Retrofit untuk akses API
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
 }
